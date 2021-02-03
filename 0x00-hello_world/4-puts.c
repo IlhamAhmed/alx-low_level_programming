6139cd95ceff16ee9cce2d4632747b14bbe08319
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-03 16:12:03 +0000

    editing 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+ * Print a string
+ * return 0
+ */
+
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }

17e56e8d3c270a485e449957dac3c2bf144d2d6a
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-03 20:40:50 +0000

    editing 4 again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-/*
- * Print a string
- * return 0
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";

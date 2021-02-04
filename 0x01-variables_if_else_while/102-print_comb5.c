b8b3a5783fad9ac4b6b71fa3b3e42a71e0600e87
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-04 20:24:35 +0000

    final commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,22 +14,19 @@ int main(void)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			for (k = 48; k < 58; k++)
+			for (k = i; k < 58; k++)
 			{
-				for (m = 48; m < 58; m++)
+				for (m = j + 1; m < 58; m++)
 				{
-					if (k >= i && m > j)
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if ((i != 57 || j != 56) || (k != 57 || m != 57))
 					{
-						putchar(i);
-						putchar(j);
+						putchar(44);
 						putchar(32);
-						putchar(k);
-						putchar(m);
-						if (i != 57 || j != 56 || k != 57 || m != 57)
-						{
-							putchar(44);
-							putchar(32);
-						}
 					}
 				}
 			}

95f0777c440da19d79b73bcf60281a5abf7afd54
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-09 21:49:22 +0000

    answer for 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - prints the word holberton
+ *Return: 0 for success
+ */
+int main(void)
+{
+  putchar(72);
+  putchar(111);
+  putchar(108);
+  putchar(108);
+  putchar(98);
+  putchar(101);
+  putchar(114);
+  putchar(116);
+  putchar(111);
+  putchar(110);
+  putchar(10);
+  return(0);
+}
+

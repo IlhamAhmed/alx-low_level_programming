e8af94409a4599bca596b30cb86594a0fa7d75dc
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-10 12:31:43 +0000

    editing 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- *main - prints the word holberton
- *Return: 0 for success
+ *main - prints holberton
+ *Return: 0
  */
 int main(void)
 {
-  putchar(72);
-  putchar(111);
-  putchar(108);
-  putchar(108);
-  putchar(98);
-  putchar(101);
-  putchar(114);
-  putchar(116);
-  putchar(111);
-  putchar(110);
-  putchar(10);
-  return(0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
-
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/**
+ * _putchar - writes character to standard output
+ * @c: The character to print
+ */
+int _putchar(char c);
+#endif

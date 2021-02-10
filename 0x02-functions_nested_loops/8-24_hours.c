0a93cd8fd81cb138fb537b76154e341e307374b3
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-10 17:11:51 +0000

    editing 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,25 +8,17 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	int k;
-	int m;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (m = 0; m <= 9; m++)
-				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(58);
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(58);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 
 	}

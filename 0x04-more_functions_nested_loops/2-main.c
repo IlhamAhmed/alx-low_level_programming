7f52e8cc3730577d7d3bb6b8d62c562a0e6c8833
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-11 12:58:10 +0000

    editing 2

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
+	int p;
+
+	p = mul(98, 1024);
+	printf("%d\n", p);
+	p = mul(-402, 4096);
+	printf("%d\n", p);
 	return (0);
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,5 +7,8 @@
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int p;
+
+	p = a * b;
+	return (p);
 }

486bf71f01ac1e0379aca4d04dadb869778079b1
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-16 19:40:38 +0000

    answer for 8

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+int main(void)
+{
+	int aray[5];
+
+	aray[0] = 98;
+	aray[1] = 402;
+	aray[2] = -198;
+	aray[3] = 298;
+	aray[4] = -1024;
+	print_array(aray, 5);
+	return (0);
+}

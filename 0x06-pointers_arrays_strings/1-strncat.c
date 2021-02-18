8cfd08509bafe3a93715159f5f638c1fe013567e
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-18 20:40:44 +0000

    answer for 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,5 +6,33 @@
  *@n: takes int
  *Return: a string
  */
-char *_strncat(char *dest, char *srcc, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+	int i;
+	int len;
+
+	len = strLen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
+/**
+ *strLen - finds length of a string
+ *@c: takes a string
+ *Return: int
+ */
+int strLen(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}

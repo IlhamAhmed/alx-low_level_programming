9de95e98eef2c6ebe51558c0dee7e72771cfeacd
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-19 12:50:26 +0000

    answer for 4

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@ int strLen(char *c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void print_array(int *a, int n);
 void reverse_array(int *a, int n);
 #endif

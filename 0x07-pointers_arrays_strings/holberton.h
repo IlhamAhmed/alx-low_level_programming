93c0852e3b3ef4e1da8153d697f43f14d5ff9b9d
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-22 15:29:51 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+void simple_print_buffer(char *buffer, unsigned int size);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+#endif

34f699595f264cd61ed50fbc9604015d2b6fd36d
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-23 20:46:45 +0000

    answer for 6

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_String(char **s, char *to);

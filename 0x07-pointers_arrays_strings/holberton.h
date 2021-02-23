7a8920b0bbf12d6d70951770fd9f76debdc4b930
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-23 10:09:47 +0000

    answer for 4

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 void simple_print_buffer(char *buffer, unsigned int size);

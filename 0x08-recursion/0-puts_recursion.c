aa97e9294c72c75918c03b12e7d64e01de2b03c1
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-24 19:46:24 +0000

    answer for 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - prints a string recursively
+ *@s: take a string
+ *Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+}

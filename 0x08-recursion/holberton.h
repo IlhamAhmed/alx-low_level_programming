70c85f0a98f3fb5333b2c2791261c6b87719eece
IlhamAhmed <ilhamahmed042@gmail.com>
2021-02-25 10:58:57 +0000

    answer for 7

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,5 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int is_pal(char *s, int i, int j);
+int is_palindrome(char *s);
+int is_prime_number(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _strlen_recursion(char *s);

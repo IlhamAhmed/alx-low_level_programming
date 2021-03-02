6bf79ab71f9766ba3880e83841e1ff6efc2f1d4b
IlhamAhmed <ilhamahmed042@gmail.com>
2021-03-02 09:05:54 +0000

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#define NULL 0
+/**
+ **create_array - creates an array of chars and initialize with specific char
+ *@size: takes the size
+ *@c: takes the character
+ *Return: the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(char) * size);
+	if (s = NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+#endif

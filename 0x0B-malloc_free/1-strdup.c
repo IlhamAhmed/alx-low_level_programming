6db8d6bcf74a83aadf4c1e5d1d6168f156a1945b
IlhamAhmed <ilhamahmed042@gmail.com>
2021-03-02 10:34:31 +0000

    answer for 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ **_strdup - returns a pointer to a copy of a string
+ *@str: takes the string
+ *Return: a pointer to char
+ */
+char *_strdup(char *str)
+{
+	char *st;
+	int i = 0;
+	int size = _strlen(str) + 1;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	st = malloc(sizeof(char) * size);
+	if (st == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		st[i] = str[i];
+		i++;
+	}
+	return (st);
+}
+/**
+ *_strlen - returns the length of a string
+ *@s: takes char parameter
+ *Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = *s;
+	while (c != '\0')
+	{
+		i++;
+		c = *(s + i);
+	}
+	return (i);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -3,4 +3,6 @@
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strdup(char *str);
+int _strlen(char *s);
 #endif

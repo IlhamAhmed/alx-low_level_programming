542ead6200000e3531ed1fd9a2cad86ef26574e6
IlhamAhmed <ilhamahmed042@gmail.com>
2021-03-09 16:12:51 +0000

    editing 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,14 +10,75 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogg;
+	char *new_name;
+	char *new_owner;
 
 	dogg = malloc(sizeof(struct dog));
 	if (dogg == NULL)
 	{
 		return (NULL);
 	}
-	dogg->name = name;
+	new_name = malloc(_strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(dogg);
+		return (NULL);
+	}
+	new_owner = malloc(_strlen(owner) + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(dogg);
+		return (NULL);
+	}
+	new_name = _strcpy(new_name, name);
+	new_owner = _strcpy(new_owner, owner);
+	dogg->name = new_name;
 	dogg->age = age;
-	dogg->owner = owner;
+	dogg->owner = new_owner;
 	return (dogg);
+}
+/**
+ *_strlen - returns the length of a string
+ *@s: takes char parameter
+ *Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = *s;
+	while (c != '\0')
+	{
+		i++;
+		c = *(s + i);
+	}
+	return (i);
+}
+/**
+ **_strcpy - copies a string
+ *@dest: takes astring
+ *@src: takes the source string
+ *Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l;
+	char c;
+
+	l = 0;
+	c = src[0];
+	while (c != '\0')
+	{
+		l++;
+		c = src[l];
+	}
+	for (i = 0; i <= l; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,8 @@ struct dog
 	float age;
 	char *owner;
 };
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);

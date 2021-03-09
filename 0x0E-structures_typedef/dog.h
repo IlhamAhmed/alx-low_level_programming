4cd63ec1cf81380d2183df5cf4ba8305847a1472
IlhamAhmed <ilhamahmed042@gmail.com>
2021-03-09 14:29:21 +0000

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+/**
+ * struct dog - it is a struct called dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ *Description: This struct defines a type called dof
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
